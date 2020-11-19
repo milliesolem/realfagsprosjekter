647dcaeb129f0818f4f68d2071bf6593e120aac2
William L. Solem <61805186+williamsolem@users.noreply.github.com>
2020-11-19 21:38:27 +0100

    Pushing new Particles In A Box

--- ParticlesInABox/ParticlesInABox.py
+++ ParticlesInABox/ParticlesInABox.py
@@ -1,78 +1,97 @@
+import numpy as np
 import random
 
 class Particle:
-    def __init__(self,x=0,y=0,z=0,vx=0,vy=0,vz=0):
-        self.x = float(x)
-        self.y = float(y)
-        self.z = float(z)
+    def __init__(self,r,v):
+        self.r = np.array(r).astype(np.float_)
+        self.v = np.array(v).astype(np.float_)
+    def move(self,dt,a):
+        self.v += a*dt
+        self.r += self.v*dt
 
-        self.vx = float(vx)
-        self.vy = float(vy)
-        self.vz = float(vz)
-    def move(self,dt,ax,ay,az):
-        self.vx += ax*dt
-        self.vy += ay*dt
-        self.vz += az*dt
-        
-        self.x += self.vx*dt
-        self.y += self.vy*dt
-        self.z += self.vz*dt
+    def collide(self,lmin,lmax):
+        self.mask = (lmin<=self.r) | (lmax>=self.r)
+        self.v[self.mask] *= -1
 
-    def collide(self,Lx1,Lx2,Ly1,Ly2,Lz1,Lz2):
-        if self.x>=Lx1 or self.x<=Lx2:
-            self.vx = -self.vx
-        if self.y>=Ly1 or self.x<=Ly2:
-            self.vy = -self.vy
-        if self.z>=Lz1 or self.x<=Lz2:
-            self.vz = -self.vz
+class Measurement:
+    def __init__(self, **kwargs):
+        self.properties = kwargs
+        self.results = dict()
+    def measure(self, particles):
+        pass
 
 class Box:
-    def __init__(self,Lx2 = 1,Ly2=None,Lz2=None,Lx1=0,Ly1=0,Lz1=0):
-        if Ly2 is None:
-            Ly2 = Lx2
-        if Lz2 is None:
-            Lz2 = Lx2
-        self.Lx2 = float(Lx2)
-        self.Ly2 = float(Ly2)
-        self.Lz2 = float(Lz2)
+    def __init__(self,dim,lmin = None, lmax = None):
+        self.dim = int(dim)
+        
 
-        self.Lx1 = float(Lx1)
-        self.Ly1 = float(Ly1)
-        self.Lz1 = float(Lz1)
-    def generate_particles(self, n, mass, kT, mu=0):
-        self.particles = []
-        self.kT = float(kT)
+        if lmin is None and lmax is None:
+            self.lmin = np.zeroes(self.dim)
+            self.lmax = np.ones(self.dim)
+        elif lmin is None and lmax is not None:
+            self.lmax = np.array(lmax).astype(np.float_)
+            self.lmin = self.lmax - 1
+        else:
+            self.lmin = np.array(lmin).astype(np.float_)
+            self.lmax = self.lmin + 1
+
+        assert self.lmin.size==self.dim, f"'lmin'  has dimension {self.lmin.size}, but {self.dim} dimensions expected"
+        assert self.lmax.size==self.dim, f"'lmax'  has dimension {self.lmax.size}, but {self.dim} dimensions expected"
+        assert np.all(self.lmin<self.lmax), f"lmin cannot be greater or equal to lmax"
+
+        
+    def generate_particles(self, N, beta=1, mass=1, mu=0):
+        self.N = int(N)
+        self.beta = float(beta)
         self.mass = float(mass)
         self.mu = float(mu)
-        
-        self.sigma = (self.kT/self.mass)**0.5
-        
-        for i in range(n):
+        self.sigma = (self.mass*self.beta)**(-0.5)
+
+        dl = self.lmax - self.lmin
+        self.particles = []
+        for i in range(self.N):
             
-            P = Particle(
-                x = random.uniform(self.Lx1,self.Lx2),
-                y = random.uniform(self.Ly1,self.Ly2),
-                z = random.uniform(self.Lz1,self.Lz2),
+            r0 = self.lmin+dl*np.random.uniform(size=self.dim)
+            v0 = self.mu + self.sigma * np.random.randn(self.dim)
 
-                vx = random.gauss(self.mu,self.sigma),
-                vy = random.gauss(self.mu,self.sigma),
-                vz = random.gauss(self.mu,self.sigma)
-            )
-            self.particles.append(P)
+            p = Particle(r = r0, v = v0)
+            self.particles.append(p)
 
-    def simulate(dt,n):
+    def simulate(self,  dt, n, *Measurements, a=0, t0 = 0):
         self.dt = float(dt)
-        self.n = float(n)
+        self.n = int(n)
+        self.t0 = float(t0)
+
+        t = self.t0 + self.dt*np.linspace(0,self.n,self.n+1)
+
+        A = a if callable(a) else lambda t: a
         for i in range(n):
+
             for p in self.particles:
-                p.move(dt,0,0,0)
-                p.collide(
-                    self.Lx1,self.Lx2,
-                    self.Ly1,self.Ly2,
-                    self.Lz1,self.Lz2,
-                )
+                p.move(dt, A(t[i]))
+                p.collide(self.lmin,self.lmax)
+                
+            for m in Measurements:
+                m.measure(self.particles)
+        
+    
         
         
+
+class CountCollisions (Measurement):
+    def __init__(self):
+        super().__init__(N_collisions = 0)
+    def measure(self, particles):
+        for p in particles:
+            self.properties["N_collisions"]+=np.any(p.mask)
         
-            
+if __name__ == "__main__":
+    A = Box(dim = 2,lmax = [-1,-1])
+    A.generate_particles(100)
+
+    C = CountCollisions()
+    
+    A.simulate(1e-4, 10000, C)
+
+    print(C.properties)
         
